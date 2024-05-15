0d77906524d14de6e6fcbdf34c6845ddf7ebc858
Mauritz Marais <mauritz303@gmail.com>
2024-05-15 15:48:46 +0200

    added event listener to close mobile nav when a link is clicked

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -5,7 +5,16 @@ const menuToggle = document.querySelector(".cs-toggle");
 
 // add aria expanded properties*
 
+// remove active class when clicking on a mobile link
+window.addEventListener("click", (e) => {
+     if (e.target.matches("a")) {
+          menuToggle.classList.remove("cs-active");
+          navigation.classList.remove("cs-active");
+     }
+});
+
 menuToggle.addEventListener("click", () => {
      menuToggle.classList.toggle("cs-active");
      navigation.classList.toggle("cs-active");
 });
+
