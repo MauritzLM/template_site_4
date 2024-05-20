91fbc9de5cf3f0fb20e3747bbdaac7a1019fed9f
Mauritz Marais <mauritz303@gmail.com>
2024-05-20 13:19:50 +0200

    added aria expanded property

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -2,6 +2,7 @@
 // Navigation toggle
 const navigation = document.querySelector("#cs-navigation");
 const menuToggle = document.querySelector(".cs-toggle");
+const ul = document.querySelector(".nav-ul-wrapper");
 
 // add aria expanded properties*
 
@@ -17,4 +18,15 @@ menuToggle.addEventListener("click", () => {
      menuToggle.classList.toggle("cs-active");
      navigation.classList.toggle("cs-active");
 });
+
+// function to set aria
+function setAria() {
+     const isExpanded = ul.getAttribute("aria-expanded")
+
+     if (isExpanded) {
+          ul.setAttribute("aria-expanded", "false");
+     } else {
+          ul.setAttribute("aria-expanded", "true");
+     }
+};
 
